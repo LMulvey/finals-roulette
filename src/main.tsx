f75d14aa10ae15ae9abea25826db5bef0eccf86f
Lee Mulvey <lmulvey@me.com>
2025-01-18 23:19:37 -0700

    feat: fix build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App';
+import { StrictMode } from 'react';
+// eslint-disable-next-line import/no-unassigned-import
+import './index.css';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.querySelector('#root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
