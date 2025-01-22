2d6108da3e694ef82fbdfa1d05dc6a29408f2b52
Lee Mulvey <lmulvey@me.com>
2025-01-22 16:49:07 -0700

    test: testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import vike from 'vike/plugin'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), vike()],
+  plugins: [tsconfigPaths(), react(), vike({prerender: true}), vercel({
+      source: '/.*'
+    })],
 });
