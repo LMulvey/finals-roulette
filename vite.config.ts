96e7fa9dd6153c844cb5fd799cce71bd694a2b0c
Lee Mulvey <lmulvey@me.com>
2025-01-22 16:49:39 -0700

    fix: remove unused deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import vike from 'vike/plugin'
 import tsconfigPaths from 'vite-tsconfig-paths';
-import vercel from 'vite-plugin-vercel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), vike({prerender: true}), vercel({
-      source: '/.*'
-    })],
+  plugins: [tsconfigPaths(), react(), vike()],
 });
