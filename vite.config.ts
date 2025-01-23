d9a797c167d9f9d893f530930f020e29bf663ccb
Lee Mulvey <lmulvey@me.com>
2025-01-22 17:52:21 -0700

    fix: vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import vike from 'vike/plugin'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT as unknown as number,
+  plugins: [tsconfigPaths(), react(), vike({prerender: true}), vercel()],
+  vercel: {
+    expiration: 25,
   },
-  plugins: [tsconfigPaths(), react(), vike()],
 });
