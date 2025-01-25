5fdea4d71d4dce2549b8c9e095c94f7cc5d14225
Lee Mulvey <lmulvey@me.com>
2025-01-25 09:59:33 -0700

    feat: fix bug

--- src/pages/saved/+Page.tsx
+++ src/pages/saved/+Page.tsx
@@ -43,7 +43,6 @@ export const Page = () => {
           <LoadoutDisplay
             key={serializeLoadout(loadout)}
             loadout={loadout}
-            showToolbar
           />
         ) : null}
       </div>
